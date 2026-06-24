6444e6e84edb487146769cbd1067bfd0099b568d
icebrainrants-tech <icebrainrants@gmail.com>
2026-06-23 21:04:00 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,8 @@
+local lighting = game:GetService("Lighting")
+local sky = lighting:FindFirstChildOfClass("Sky") or Instance.new("Sky", lighting)
+sky.SkyboxBk = "rbxassetid://125243715955652"
+sky.SkyboxDn = "rbxassetid://125243715955652"
+sky.SkyboxFt = "rbxassetid://125243715955652"
+sky.SkyboxLf = "rbxassetid://125243715955652"
+sky.SkyboxRt = "rbxassetid://125243715955652"
+sky.SkyboxUp = "rbxassetid://125243715955652"
